6a7d8a9a96e083c159589af89022d40bdb85fd0b
Roberto Moretti <RobertoMoretti@MacBook-Air-di-Roberto-2.local>
2018-05-09 16:33:10 +0200

    minor fixes

--- AnaSiPm.C
+++ AnaSiPm.C
@@ -23,43 +23,43 @@ void AnaSiPm::Loop()
 	int debug=0;
 	vector<int> PixelAcceso(NPixTot,0.);
 	vector<int> FiredPixels;
-//	int PixelAcceso[NPixTot]=0;
+	//	int PixelAcceso[NPixTot]=0;
 	int ii=0;
 	vector<int> Counter(nentries,0.);
 	
-		std::vector<int>::iterator it;
+	std::vector<int>::iterator it;
 	
 	Long64_t nbytes = 0, nb = 0;
 	//	nentries/=10; //to go quickly
-//	nentries=10;
-	for (Long64_t jentry=0; jentry<nentries;jentry++) {
+	//	nentries=10;
+	for (Long64_t jentry=0; jentry<nentries;jentry++) { // INIZIO CICLO SULLE PARTICELLE PRIMARIE
 		
 		if (jentry%(nentries/10)==0) cout<<"Sto analizzando l'evento "<<jentry<<" di "<<nentries<<": %= "<<100*jentry/nentries<<endl;
-
+		
 		Long64_t ientry = LoadTree(jentry);
 		if (ientry < 0) break;
 		nb = fChain->GetEntry(jentry);   nbytes += nb;
 		// if (Cut(ientry) < 0) continue;
 		
-
+		
 		for (ii=0; ii<PixelID->size(); ii++){
 			if (debug) cout<<" Evento n= "<<jentry<<" ii= "<<ii <<" Pixel acceso: "<< PixelID->at(ii)<<endl;
 			it = find(FiredPixels.begin(), FiredPixels.end(),PixelID->at(ii));
 			if (it == FiredPixels.end()) {
-			FiredPixels.push_back(PixelID->at(ii));
+				FiredPixels.push_back(PixelID->at(ii));
 				if (debug) cout<<" Nuovo, aggiungo!"<<endl;
 			} else if (debug) cout<<" C'era gia, stica!"<<endl;
 			
-//			PixelAcceso[PixelID->at(ii)]=1;
+			//			PixelAcceso[PixelID->at(ii)]=1;
 			
 			
 		}
 		
-//		Counter[jentry]=std::count(PixelAcceso.begin(), PixelAcceso.end(), 1);
+		//		Counter[jentry]=std::count(PixelAcceso.begin(), PixelAcceso.end(), 1);
 		Counter[jentry]=FiredPixels.size();
 #if 0
 		if (debug) cout<<" Evento n= "<<jentry<<" elenco pixel accesi: "<<endl;
-
+		
 		for (ii=0; ii<NPixTot; ii++) {
 			if (PixelAcceso[ii]!=0) {
 				if (debug) cout<< ii<<endl;
@@ -70,14 +70,14 @@ void AnaSiPm::Loop()
 		if (debug)		cout<<"                     "<<jentry<<" numero pixel accesi: "<<Counter[jentry] <<endl;
 		NPixel->Fill(Counter[jentry]);
 		
-
-//		PixelAcceso.assign(PixelAcceso.size(),0);
+	
+		//		PixelAcceso.assign(PixelAcceso.size(),0);
 		FiredPixels.clear();
 		/*
-		for (ii=0; ii<NPixTot; ii++) {
-			if (PixelAcceso[ii]==1) PixelAcceso[ii]=0;
-		}
-		*/
+		 for (ii=0; ii<NPixTot; ii++) {
+		 if (PixelAcceso[ii]==1) PixelAcceso[ii]=0;
+		 }
+		 */
 		
 		
 		
@@ -90,8 +90,8 @@ void AnaSiPm::Loop()
 		
 		
 		
-	} // FINE CICLO SUL PRIMARIO
-
+	} // FINE CICLO SULLE PARTICELLE PRIMARIE
+	
 	NPixel->Draw();
 	NPixel->Write();
 	
